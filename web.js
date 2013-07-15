c3fd5fb592e614d63d269bbe51c4a03f2d36b8dc
Jonathan Ku <jonku720@gmail.com>
2013-07-14 20:45:49 -0400

    read index.html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
-var express = require('express');
+var express = require('express'), 
+	fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var output = new Buffer(fs.readFileSync('index.html'));
+	response.send(output.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
